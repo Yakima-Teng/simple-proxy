376b061da94c02f7e650b713dcc5f64dbb583673
Yakima <cleveryun@163.com>
2020-04-07 22:32:27 +0800

    small adjustment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,24 @@
 const config = require('./config')
 
 module.exports = {
-    apps : [{
-        name: `simple-proxy-${config.port}`,
-        script: './app.js',
+    apps: [
+        {
+            name: `simple-proxy-${config.port}`,
+            script: './app.js',
 
-        // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-        args: 'by Yakima',
-        instances: 1,
-        autorestart: true,
-        watch: false,
-        max_memory_restart: '200M',
-        env: { // pm2 start ecosystem.config.js
-            NODE_ENV: 'production',
+            // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+            args: 'by Yakima',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '200M',
+            env: { // pm2 start ecosystem.config.js
+                NODE_ENV: 'production',
+            },
+            error_file: 'logs/err.log',
+            out_file: 'logs/out.log',
+            log_file: 'logs/combined.log',
+            time: true,
         },
-        error_file: 'logs/err.log',
-        out_file: 'logs/out.log',
-        log_file: 'logs/combined.log',
-        time: true,
-    }],
+    ],
 }
